e526db146ee3cb8d0663d994e2ae4a35051f5151
Karim-Bouqsi <pro.karimb@gmail.com>
2025-03-05 11:19:03 +0100

    Implémentation de la page principale

---
+++ lib/vue/home_view.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class HomeView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/background_HomePage.png"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                "Bienvenue dans Clicker Game!",
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.white),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/game'); // redirection vers le jeu
+                },
+                child: const Text("Jouer"),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
