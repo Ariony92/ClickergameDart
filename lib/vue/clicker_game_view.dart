96e1ae0526f013badae7e1918f5c0bd93f9097a6
hrahman <hamidur.rahman@outlook.fr>
2025-03-13 14:52:20 +0100

    update: cration widget joueur

--- lib/vue/clicker_game_view.dart
+++ lib/vue/clicker_game_view.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../view_modele/ennemis_viewmodele.dart';
 import '../view_modele/joueur_viewmodele.dart';
+import '../widgets/joueur_widget.dart';
+import '../widgets/enemy_widget.dart';
 
 class ClickerGameView extends StatefulWidget {
   @override
@@ -32,140 +34,9 @@ class _ClickerGameViewState extends State<ClickerGameView> {
               : Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Text(
-                      "Niveau ${ennemisViewModel.niveauActuel}",
-                      style: TextStyle(
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white),
-                    ),
-                    SizedBox(height: 15),
-                    
-                    Text(
-                      ennemisViewModel.ennemiActuel!.nom,
-                      style: TextStyle(
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black),
-                    ),
-                    SizedBox(height: 15),
-
-                    // Barre de vie de l'ennemi
-                    Container(
-                      width: 200,
-                      height: 20,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(5),
-                        color: Colors.grey[300],
-                      ),
-                      child: FractionallySizedBox(
-                        alignment: Alignment.centerLeft,
-                        widthFactor: (ennemisViewModel.ennemiActuel!.vieActuelle /
-                                ennemisViewModel.ennemiActuel!.vieTotale)
-                            .clamp(0.0, 1.0),
-                        child: Container(
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(5),
-                            color: Colors.red,
-                          ),
-                        ),
-                      ),
-                    ),
-                    const SizedBox(height: 10),
-
-                    Text(
-                      "PV Ennemi : ${ennemisViewModel.ennemiActuel!.vieActuelle} / ${ennemisViewModel.ennemiActuel!.vieTotale}",
-                      style:
-                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                    ),
-                    const SizedBox(height: 20),
-
-                    GestureDetector(
-                      onTap: () {
-                        int degatsInfliges = joueurViewModel.cliquer();
-                        ennemisViewModel.attaquerEnnemi(
-                            degatsInfliges, joueurViewModel, context);
-                      },
-                      child: ennemisViewModel.ennemiActuel!.image
-                              .startsWith("assets/")
-                          ? Image.asset(
-                              ennemisViewModel.ennemiActuel!.image,
-                              width: 200,
-                              height: 200,
-                              errorBuilder: (context, error, stackTrace) {
-                                return Text("Erreur chargement image");
-                              },
-                            )
-                          : Image.network(
-                              ennemisViewModel.ennemiActuel!.image,
-                              width: 200,
-                              height: 200,
-                              errorBuilder: (context, error, stackTrace) {
-                                return Text("Erreur chargement image");
-                              },
-                            ),
-                    ),
-
+                    EnemyWidget(),
                     const SizedBox(height: 30),
-
-                    // Barre de vie du joueur
-                    Text(
-                      "Votre Vie",
-                      style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                    ),
-                    Container(
-                      width: 200,
-                      height: 20,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(5),
-                        color: Colors.grey[300],
-                      ),
-                      child: FractionallySizedBox(
-                        alignment: Alignment.centerLeft,
-                        widthFactor: (joueurViewModel.vieActuelle /
-                                joueurViewModel.vieMax)
-                            .clamp(0.0, 1.0),
-                        child: Container(
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(5),
-                            color: Colors.green, // Vert pour le joueur
-                          ),
-                        ),
-                      ),
-                    ),
-                    const SizedBox(height: 10),
-                    Text(
-                      "PV : ${joueurViewModel.vieActuelle} / ${joueurViewModel.vieMax}",
-                      style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                    ),
-
-                    const SizedBox(height: 20),
-
-                    Text(
-                      "Expérience : ${joueurViewModel.experience}",
-                      style: TextStyle(fontSize: 18),
-                    ),
-                    Text(
-                      "Dégâts par clic : ${joueurViewModel.degats}",
-                      style: TextStyle(fontSize: 18),
-                    ),
-                    const SizedBox(height: 10),
-
-                    ElevatedButton(
-                      onPressed: () {
-                        int result = joueurViewModel.ameliorerDegats();
-                        if (result == -1) {
-                          ScaffoldMessenger.of(context).showSnackBar(
-                            SnackBar(
-                              content: Text("Pas assez d'expérience !"),
-                              duration: Duration(seconds: 1),
-                            ),
-                          );
-                        }
-                      },
-                      child: Text(
-                          "Améliorer dégâts (${joueurViewModel.coutAmeliorationDegats()} XP)"),
-                    ),
+                    JoueurWidget(),
                   ],
                 ),
         ),
